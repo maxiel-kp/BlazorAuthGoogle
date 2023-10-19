f8872574b9524dbbcf96ca51e02d25acb78a61ad
kunlp885 <Kunlapath.Paengsa@innopiaglobal.com>
2023-10-20 00:38:30 +0700

    Add project files.

---
+++ BlazorAuthGoogle/Program.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAuthGoogle;
+using Microsoft.AspNetCore.Authentication.Google;
+
+namespace BlazorAuthGoogle
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+            });
+
+
+            //Google
+            builder.Services.AddApiAuthorization
+
+             await builder.Build().RunAsync();
+        }
+    }
+}
